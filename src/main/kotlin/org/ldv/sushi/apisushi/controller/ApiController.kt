7490793ff7998e48d1484267e0817fdf950e54bc
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2023-03-23 10:02:25 +0100

    update image name saumon -> salmon

--- src/main/kotlin/org/ldv/sushi/apisushi/controller/ApiController.kt
+++ src/main/kotlin/org/ldv/sushi/apisushi/controller/ApiController.kt
@@ -5,6 +5,7 @@ import org.ldv.sushi.apisushi.repository.BoxRepository
 import org.ldv.sushi.apisushi.util.fromBoxToBoxDtoJson
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
